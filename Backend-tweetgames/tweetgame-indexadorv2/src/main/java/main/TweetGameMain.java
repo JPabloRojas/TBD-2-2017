7d0e98145e8e591ee9e1812a5c5c4c5bfe5b2e8d
JPabloRojas <34076111+JPabloRojas@users.noreply.github.com>
2017-11-28 18:41:16 -0300

    Update TweetGameMain.java

--- Backend-tweetgames/tweetgame-indexadorv2/src/main/java/main/TweetGameMain.java
+++ Backend-tweetgames/tweetgame-indexadorv2/src/main/java/main/TweetGameMain.java
@@ -28,18 +28,12 @@ public class TweetGameMain {
         tweetgame_core tc = new tweetgame_core();
         tc.beginProcess();
 
-        //MySQLDB db = new MySQLDB();
-        //db.updateCountry("test",(float)-5.232,(float)70.231);
-
-
+        
         //2: Inicial controladores de neo4j
-        //neo4j n4 = new neo4j("bolt://localhost:7687","neo4j","123");
-        //n4.createNodes();
-        //n4.obtaintopUser();
-
-        //geoLocation geo = new geoLocation();
-        //System.out.println(geo.getTweetCountry("https://maps.googleapis.com/maps/api/geocode/json?latlng=40.3577264,-2.4757481&key=AIzaSyDPcBuAO5CwFUoXwj6aoX2poU8JVFR1eic"));
-        //geo.updateTweetsCount();
+        neo4j n4 = new neo4j("bolt://localhost:7687","neo4j","123");
+        n4.createNodes();
+        n4.obtaintopUser();
+  
 
 
     }
